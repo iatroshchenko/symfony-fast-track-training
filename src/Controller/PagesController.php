8ada3dd8c38cf50d96e3ca10fafd276620d63bb2
iatroshchenko <iatrodev@gmail.com>
2020-08-26 18:25:23 +0300

    Twig template render

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -2,28 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class PagesController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(Environment $twig, ConferenceRepository $repository)
     {
-        $response = "
-            <html>
-        <body>
-        <div style='text-align: center;'>
-        <h1>This site is under construction</h1>
-        <img src='/images/under-construction.gif' alt='under construction'>
-</div>
-</body>    
-</html>
-        ";
-
-        return new Response($response);
+        return new Response($twig->render('pages/index.html.twig', [
+            'conferences' => $repository->findAll()
+        ]));
     }
 }
